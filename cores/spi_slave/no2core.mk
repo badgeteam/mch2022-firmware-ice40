3d6602b6ac9e60b209f1878bbc9ed3e760f6ed49
Sylvain Munaut <tnt@246tNt.com>
2022-05-19 09:18:43 +0200

    cores/spi_slave: Remove useless cores
    Those were imported when copying the core over here but
    they're not used and don't match the defined SPI proto
    so just remove them.
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- cores/spi_slave/no2core.mk
+++ cores/spi_slave/no2core.mk
@@ -2,13 +2,10 @@ CORE := spi_slave
 
 RTL_SRCS_spi_slave := $(addprefix rtl/, \
 	spi_fast_core.v \
-	spi_fast.v \
-	spi_reg.v \
-	spi_simple.v \
 )
 
 TESTBENCHES_spi_slave := \
 	spi_fast_core_tb \
-	spi_tb
+	$(NULL)
 
 include $(NO2BUILD_DIR)/core-magic.mk
